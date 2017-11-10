2acebcdd904479352e832eb6b412729c3ceecaf0
Michael Bordenkircher <michaelkmarsh@gmail.com>
2017-11-10 15:16:11 -0500

    add js and images

--- js/action.js
+++ js/action.js
@@ -1,7 +1,120 @@
 console.log('hello');
-function displayInfo () {
-document.getElementById('vanillainfo').style.display = "block"
+
+var vanilla = document.getElementById('vanillainfo');
+var chocolate = document.getElementById('chocolateinfo');
+var mint = document.getElementById('mintinfo');
+var pumpkin = document.getElementById('pumpkininfo');
+var pb = document.getElementById('pbinfo');
+var cncream = document.getElementById('cncreaminfo');
+var coffee = document.getElementById('coffeeinfo');
+var raspberry = document.getElementById('raspberryinfo');
+var blueberry = document.getElementById('blueberryinfo');
+
+
+function displayVanillaInfo() {
+  chocolate.style.display = "none";
+  mint.style.display = "none";
+  pumpkin.style.display = "none";
+  pb.style.display = "none";
+  cncream.style.display = "none";
+  coffee.style.display = "none";
+  raspberry.style.display = "none";
+  blueberry.style.display = "none";
+  vanilla.style.display = "block";
 };
 
+function displayChocolateInfo() {
+  vanilla.style.display = "none";
+  mint.style.display = "none";
+  pumpkin.style.display = "none";
+  pb.style.display = "none";
+  cncream.style.display = "none";
+  coffee.style.display = "none";
+  raspberry.style.display = "none";
+  blueberry.style.display = "none";
+  chocolate.style.display = "block";
+};
 
-displayInfo();
+function displayMintInfo() {
+  vanilla.style.display = "none";
+  chocolate.style.display = "none";
+  pumpkin.style.display = "none";
+  pb.style.display = "none";
+  cncream.style.display = "none";
+  coffee.style.display = "none";
+  raspberry.style.display = "none";
+  blueberry.style.display = "none";
+  mint.style.display = "block";
+}
+
+function displayPumpkinInfo() {
+  vanilla.style.display = "none";
+  chocolate.style.display = "none";
+  mint.style.display = "none";
+  pb.style.display = "none";
+  cncream.style.display = "none";
+  coffee.style.display = "none";
+  raspberry.style.display = "none";
+  blueberry.style.display = "none";
+  pumpkin.style.display = "block";
+}
+
+function displayPbInfo() {
+  vanilla.style.display = "none";
+  chocolate.style.display = "none";
+  pumpkin.style.display = "none";
+  mint.style.display = "none";
+  cncream.style.display = "none";
+  coffee.style.display = "none";
+  raspberry.style.display = "none";
+  blueberry.style.display = "none";
+  pb.style.display = "block";
+}
+
+function displayCncreamInfo() {
+  vanilla.style.display = "none";
+  chocolate.style.display = "none";
+  pumpkin.style.display = "none";
+  pb.style.display = "none";
+  mint.style.display = "none";
+  coffee.style.display = "none";
+  raspberry.style.display = "none";
+  blueberry.style.display = "none";
+  cncream.style.display = "block";
+}
+
+function displayCoffeeInfo() {
+  vanilla.style.display = "none";
+  chocolate.style.display = "none";
+  pumpkin.style.display = "none";
+  pb.style.display = "none";
+  cncream.style.display = "none";
+  mint.style.display = "none";
+  raspberry.style.display = "none";
+  blueberry.style.display = "none";
+  coffee.style.display = "block";
+}
+
+function displayRaspberryInfo() {
+  vanilla.style.display = "none";
+  chocolate.style.display = "none";
+  pumpkin.style.display = "none";
+  pb.style.display = "none";
+  cncream.style.display = "none";
+  coffee.style.display = "none";
+  mint.style.display = "none";
+  blueberry.style.display = "none";
+  raspberry.style.display = "block";
+}
+
+function displayBlueberryInfo() {
+  vanilla.style.display = "none";
+  chocolate.style.display = "none";
+  pumpkin.style.display = "none";
+  pb.style.display = "none";
+  cncream.style.display = "none";
+  coffee.style.display = "none";
+  raspberry.style.display = "none";
+  mint.style.display = "none";
+  blueberry.style.display = "block";
+}
